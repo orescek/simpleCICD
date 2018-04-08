862e5801e18da85775c47c83d5cde83bdd176e20
orescek <17717937+orescek@users.noreply.github.com>
2018-04-08 09:44:58 +0200

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,20 +1,13 @@
 #!/bin/bash
-##########
-# Variables
-########
+##############
+# Variables ##
+##############
 
 jenkinsbackup=jenkins.tar.gz
 
-########################
-# Nice to have
-########################
-sudo apt-get install -y mc
-sudo apt-get install -y zip
-
-########################
-## JAVA
-########################
-
+#########
+## JAVA #
+#########
 sudo apt-get install -y software-properties-common python-software-properties
 echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
 sudo add-apt-repository ppa:webupd8team/java -y
@@ -23,9 +16,9 @@ sudo apt-get install oracle-java8-installer
 echo "Setting environment variables for Java 8.."
 sudo apt-get install -y oracle-java8-set-default
 
-########################
-# Jenkins
-########################
+###########
+# Jenkins #
+###########
 echo "Installing Jenkins"
 wget -q -O - http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
 sudo sh -c 'echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list'
@@ -44,9 +37,6 @@ cd /var/lib/jenkins/
 sudo sh -c 'cat jenkins.tar.gz.parta* >jenkins.tar.gz'
 sudo tar xvzf $jenkinsbackup
 sudo service jenkins start
-
 sudo rm /var/lib/jenkins/$jenkinsbackup.partaa
 sudo rm /var/lib/jenkins/$jenkinsbackup.partab
 sudo rm /var/lib/jenkins/$jenkinsbackup.partac
-
-
